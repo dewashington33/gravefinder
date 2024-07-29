8296b560c14c7a21174b4e6b15b58588662fb863
dewashington33 <dewashington@hotmail.com>
2024-07-28 20:16:24 -0400

    Initial Commit

---
+++ src/main/java/com/gravefinder/controllers/graveFinderMain.java
@@ -0,0 +1,16 @@
+package com.gravefinder.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class graveFinderMain {
+    @FXML
+    private Button btnCreateCSV;
+
+    @FXML
+    private TextField txtCSVFileName;
+
+    @FXML
+    private TextField txtURL;
+}
